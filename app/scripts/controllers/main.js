0aa2be275495762a709d6cb1d0c3a68c3d62c1fa
Pierre-Louis Peeters <pierrelouis.peeters@gmail.com>
2015-10-24 12:59:07 +0200

    Scaffold application with Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name salarySimApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the salarySimApp
+ */
+angular.module('salarySimApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
