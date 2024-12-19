d7b1959ae5d7d2d163f5a44a35cf6a7577005d39
PLPeeters <PLPeeters@users.noreply.github.com>
2024-12-19 16:59:50 +0100

    Add support for part-time and add NL and EN translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,26 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom([BrowserAnimationsModule]), provideAnimationsAsync(),
+    importProvidersFrom([BrowserAnimationsModule]), provideAnimationsAsync(), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['fr', 'nl', 'en'],
+          defaultLang: 'fr',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
   ]
 };
