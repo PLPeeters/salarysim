736172546d82c45e0cd9ce5d965a828337a00227
PLPeeters <PLPeeters@users.noreply.github.com>
2024-12-22 18:25:50 +0100

    Add unit tests

--- src/app/services/tax-calculator.service.spec.ts
+++ src/app/services/tax-calculator.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TaxCalculatorService } from './tax-calculator.service';
+import { INPUTS_TO_NET } from './inputs-to-net';
 
 describe('TaxCalculatorService', () => {
   let service: TaxCalculatorService;
@@ -12,5 +13,15 @@ describe('TaxCalculatorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  INPUTS_TO_NET.forEach(inputToNet => {
+    const input = inputToNet.input;
+    const expectedNet = inputToNet.net;
+
+    it(`should calculate a net of ${expectedNet} for situation ${input.status}/${input.workRegime.type}/${input.monthlyGrossSalary}`, () => {
+      const result = service.calculateNetSalary(input);
+      expect(result.netSalary).toBe(expectedNet);
+    });
   });
 });
